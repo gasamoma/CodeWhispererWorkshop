f0123d0e21fd6488cc03645ba72548e301bfd3c5
Gabriel Santiago Montero Mariño <gabriel_montero@hotmail.com>
2023-09-04 16:02:27 -0600

    Delete modules/must-ApiBackend/app/lambda/security/.~c9_invoke_SPeaeY.py

--- modules/must-ApiBackend/app/lambda/security/.~c9_invoke_SPeaeY.py
+++
@@ -1,9 +0,0 @@
-import os
-print("include works")
-#check if POST_AUTH is "1" or from the environ
-if os.environ.get("POST_AUTH",None) == "1":
-    POST_AUTH = True
-    print("POST_AUTH is set to True")
-else:
-    POST_AUTH = False
-    print("POST_AUTH is set to False")
