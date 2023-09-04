e46464bd1e0706088d2318a00c935905a6c2f0a6
Gabriel Montero <gasamoma@amazon.com>
2023-09-04 23:17:30 +0000

    adding basic post auth

--- modules/must-CognitoSecurity/app/lambda/post_auth.py
+++ modules/must-CognitoSecurity/app/lambda/post_auth.py
@@ -1,15 +1,20 @@
-# a basic lambda handler dor api gateway proxy
+# a basic lambda handler for Amazon cognito post authorization
 import json
 import boto3
-
+import uuid
 
 def lambda_handler(event, context):
-    print(event)
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+    # create a UUID 8 digit string
+    uuid = str(uuid4())[:8]
+    # load the dynamodb table name from os
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    # write the UUID to the table
+    table.put_item(
+        Item={
+            'uuid': uuid
+            })
+    # add the uuid to the response 
+    event['uuid'] = uuid
+    return event
+                            
