25830ad10a68b993e49db3b51110ba212f24ef1f
ElRuncho <francorafae@gmail.com>
2023-09-05 23:08:01 +0000

    uploads test

--- modules/must-FrontEndJS/web/scripts.js
+++ modules/must-FrontEndJS/web/scripts.js
@@ -1,14 +1,7 @@
-//a variable that captures the value of a input field called access - code
-const accessCode = $("#access-code");
-const apiurl = 'https://1236546123.execute-api.us-east-1.amazonaws.com/prod/api_backend';
-var bucketName = "cw-workshop";
-var bucketRegion = "us-east-1";
-var IdentityPoolId = "1234567890";
+// include https://code.jquery.com/jquery-3.6.0.min.js
 
-<<<<<<< HEAD
-//extract token from web url querystring parameters and assign it to a variable called token 
-const token = new URLSearchParams(window.location.search).get('token');
-=======
+
+
 $(document).ready(function() {
     // Document selection functionality
     const documentDropdown = $("#document-dropdown");
@@ -24,7 +17,7 @@ $(document).ready(function() {
             url: url,
             data: JSON.stringify(data),
             dataType: 'json',
-            headers: headers
+            headers: JSON.stringify(headers)
             }
         );
     }
@@ -37,62 +30,20 @@ $(document).ready(function() {
             documentDropdown.append(new Option(file, file));
         }
     }
->>>>>>> 7c027f0bfa60450b0b8706bff9b18e54a11bc5f8
 
 
-//A function that validates a cognito jwt tokenken
+    searchButton.click(function() {
+        const searchText = searchInput.val();
+        // do a api request to : 'https://grl6bha8b4.execute-api.us-east-1.amazonaws.com/prod/query_bedrock'
+        // create a
+        post('https://grl6bha8b4.execute-api.us-east-1.amazonaws.com/prod/query_bedrock', {
+            'query': searchText,
+            // get the selected document from the dropdown menu
+            'key': documentDropdown.val(),
 
-function validateJWTToken(token) {
-    var url = apiurl + '/validate';
-    var headers = {
-        'Authorization': 'Bearer ' + token
-    };
-    return post(url, {}, headers);
-}
-
-
-
-//a function that takes the value of the access code and sends it to the api endpoint  and returns the response as a json object
- function post(url, data, headers = {}) {
-     return $.ajax({
-         type: 'POST',
-         url: url,
-         data: JSON.stringify(data),
-         dataType: 'json',
-         headers: JSON.stringify(headers)
-     });
- }
- 
- // a function that uploads a file using aws sdk
-
- 
-function s3upload() {  
-        var files = document.getElementById('fileUpload').files;
-        if (files) 
-        {
-            var file = files[0];
-            var fileName = file.name;
-            var filePath = 'my-first-bucket-path/' + fileName;
-            var fileUrl = 'https://' + BUCKET_REGION + '.amazonaws.com/my-first-bucket/' +  filePath;
+        }).then(response => {
+            // show the response in the in the search results container
             
-<<<<<<< HEAD
-            s3.upload({
-                        Key: filePath,
-                        Body: file,
-                        ACL: 'public-read'
-                     }, function(err, data) {
-                         if(err) {
-                            reject('error');
-                         }
-                                    
-                        alert('Successfully Uploaded!');
-                    }).on('httpUploadProgress', function (progress) {
-                        var uploaded = parseInt((progress.loaded * 100) / progress.total);
-                        $("progress").attr('value', uploaded);
-                    });
-                }
-      };
-=======
             searchResultsContainer.empty();
             searchResultsContainer.show();
             searchResultsContainer.text(response);
@@ -131,10 +82,10 @@ function s3upload() {
         console.log("finished loading")
         // create a header Authorization with the id_token
         headers= {
-             'Authorization': 'Bearer '+id_token
+             'Authorization': id_token
          }
         // do a post request to this endpoint https://grl6bha8b4.execute-api.us-east-1.amazonaws.com/prod/get_user_files
-        post('https://gce33wbizd.execute-api.us-east-1.amazonaws.com/prod/api_backend',{}, headers).then(response => {
+        post('https://grl6bha8b4.execute-api.us-east-1.amazonaws.com/prod/login',{}, headers).then(response => {
 
             // and get the list of files from the response
             const files = response;
@@ -176,5 +127,40 @@ function s3upload() {
             login_button_fucntion(id_token);
         });
     });
+    
+    //const signed="https://deepracer-destination.s3.amazonaws.com/"
+    // a function that takes an s3 signed url as a parameter and uses it to upload a file. 
+    function uploadFile(signedUrl="https://deepracer-destination.s3.amazonaws.com/") {
+        // get the file from the input
+        const file = $("#fileUpload")[0].files[0];
+        // make a request to the signed url
+        $.ajax({
+            url: signedUrl,
+            type: 'PUT',
+            data: file,
+            processData: false,
+            contentType: false
+        });
+    }
+    
+    
+    
+    // a fucntion tha does a jquery get.
+    //function get(url) {
+    //    return $.ajax({
+    //        type: 'GET',
+    //        url: url
+    //    });
+    //}
+    
+    // call the apigateway to get a signed url. 
+    //function getSignedUrl(fileName) {
+        // do a post request to the api
+        //return get('https://grl6bha8b4.execute-api.us-east-1.amazonaws.com/prod/get_signed_url');
+    //}
+    
+ 
+ 
+    
+    
 });
->>>>>>> 7c027f0bfa60450b0b8706bff9b18e54a11bc5f8
