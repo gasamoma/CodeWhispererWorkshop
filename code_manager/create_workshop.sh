7e7d31df67fdc65cc0f5a5055370acf14d7607ac
Gabriel Montero <gasamoma@amazon.com>
2023-09-24 18:12:23 +0000

    adding code manager baseline

--- code_manager/create_workshop.sh
+++ code_manager/create_workshop.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-rm -rf lib
-pip3 install -r requirements-dev.txt --target=./lib/python
+uuid=$(uuidgen)
+git checkout -b ws/$uuid
+sed -e 's~"https://cw-workshop-domain-demo.auth.us-east-1.amazoncognito.com/login?client_id=597e5f4rfac3sprtrd943h8jdg&response_type=token&redirect_uri=https://d2mj6f7u00o6eq.cloudfront.net/index.html"~"{replace_with_cognito_url}"~g' -e 's~"https://pse9phfk51.execute-api.us-east-1.amazonaws.com/prod/api_backend"~"{replace_with_api_back_rul}"~g'  modules/must-FrontEndJS/web/scripts.bck.js > modules/must-FrontEndJS/web/scripts.js 
+sed -e "s~cw-workshop-domain-demo~cw-ws-$uuid~g" modules/must-ApiBackend/must_api_backend/must_api_backend_stack.bck.py > modules/must-ApiBackend/must_api_backend/must_api_backend_stack.py
+rm rf modules/must-FrontEndJS/web/scripts.bck.js modules/must-ApiBackend/must_api_backend/must_api_backend_stack.bck.py
+git add .
+git commit -m "first commit"
+git push origin uuid
+echo "git checkout -b ws/$uuid"
