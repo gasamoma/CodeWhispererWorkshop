21fe1455010f99f48bd741f48b70b7b8cb240959
Ezequiel Reyno <ezereyno@amazon.com>
2023-09-26 10:41:34 -0500

    Check table to validate if authroized

--- modules/must-ApiBackend/app/lambda/security/security.py
+++ modules/must-ApiBackend/app/lambda/security/security.py
@@ -11,6 +11,14 @@ else:
     POST_AUTH = False
     print("POST_AUTH is set to False")
 
-# a function that ...
+# a function that get from the event the user email and scan a table in dynamodb using that email as partition key
 def check_auth(event):
-    return False
+    # get the user email from the event which is an post authorization cognito event
+    user_email = event['requestContext']['authorizer']['claims']['email']
+    # scan the table using that email as partition key
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['POST_AUTHENTICATION_DYNAMO_TABLE_NAME']
+    table = dynamodb.Table(table_name)
+    response = table.query(KeyConditionExpression=Key('user-email').eq(user_email))
+    # compare the timestamp from the response date field with the current time and evaluate if is less than 60 minutes
+    return response['Items'][0]['date'] > (time.time() - 60*60)
