44b10c201c7055ec79096f4d06ad67b76bd74f23
Duvan Silva <duvanfesiro@gmail.com>
2023-09-26 15:56:16 +0000

    Adding logic to check face

--- modules/must-ApiBackend/app/lambda/api.py
+++ modules/must-ApiBackend/app/lambda/api.py
@@ -20,11 +20,51 @@ def handler(event, context):
     security.POST_AUTH = security.check_auth(event)
     print(security.POST_AUTH )
     # return the default lambda response with cors
-    response = {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-            },
-        'body': json.dumps({'Hello':"from lambda"})
-        }
+    
+   # identify if a person has its mouth open with amazon rekognition, the key of fline in s3 is located in event.body.key
+    if security.POST_AUTH:
+        print("security check passed")
+        # get the key of the image
+        key = event['body']['key']
+        # get the bucket name
+        bucket = environ.get('BUCKET');
+        
+        # create the s3 client
+        s3 = boto3.client('s3')
+        # get the image from s3 to use  with rekognition
+        image = s3.get_object(Bucket=bucket, Key=key)
+       
+        # create the rekognition client
+        rekognition = boto3.client('rekognition')
+        
+        #use image to detect faces with open mounth
+        response = rekognition.detect_faces(Image={'Bytes': image}, Attributes=['ALL'])
+        
+        # get the face
+        face = response['FaceDetails'][0]
+        
+        #check if the face has open mounth
+        # fix the syntax error in the next lines
+        if face['MouthOpen']['Value'] == True:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                    "approved": True
+                })
+            }
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                    "approved": False
+                })
+            }
+    else:
+            # return the confidence
+            return {
+                'statusCode': 403,
+                'body': json.dumps({
+                    "passToMars": False
+                })
+            }
+                
