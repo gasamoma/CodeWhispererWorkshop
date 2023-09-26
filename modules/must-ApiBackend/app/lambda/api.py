d1767b1819d3e3ba974e4d84f469166f74f44e81
davfsand <dfsandoval91@gmail.com>
2023-09-26 11:03:32 -0500

    Update api.py
    Update api

--- modules/must-ApiBackend/app/lambda/api.py
+++ modules/must-ApiBackend/app/lambda/api.py
@@ -13,12 +13,18 @@ logger = logging.getLogger(__name__)
 
 # Get the confidence.
 min_confidence = int(environ.get('CONFIDENCE', 70))
+bucket = environ.get('BUCKET')
+
 # a lambda handler for the api gateway post
 def handler(event, context):
     ## SECURITY CHECK
     print(security.POST_AUTH )
     security.POST_AUTH = security.check_auth(event)
     print(security.POST_AUTH )
+    
+    # detect faces function
+    result = detect_mouth_open(bucket, event['key'])
+
     # return the default lambda response with cors
     response = {
         "statusCode": 200,
@@ -26,5 +32,34 @@ def handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
             },
-        'body': json.dumps({'Hello':"from lambda"})
+        'body': json.dumps({'is_valid':result})
         }
+
+# create a function that detect faces from an image uploaded to an s3 bucket
+#def detect_faces(bucket, key):
+#    rekognition = boto3.client('rekognition')
+#   response = rekognition.detect_faces(
+#        Image={
+#            'S3Object': {
+#                'Bucket': bucket,
+#                'Name': key,
+#                }
+#        }
+ #   )
+
+# create a function that detects the mouth open on an image uploaded to an s3 bucket
+def detect_mouth_open(bucket, key):
+    rekognition = boto3.client('rekognition')
+    response = rekognition.detect_faces(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key,
+                }
+                },
+                Attributes=['ALL']
+                )
+    # print all attributes
+    print(response)
+    # return the mouth open value
+    return response['FaceDetails'][0]['MouthOpen']['Value']
